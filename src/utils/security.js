2895ea72fef5ea53d57e971a2c74eeace3fe5717
wellingtons666 <wellington.cnstntbtc@gmail.com>
2026-02-20 23:13:10 -0300

    Update security.js

--- src/utils/security.js
+++ src/utils/security.js
@@ -1,5 +1,5 @@
-const config = require('../config');
-const logger = require('./logger');
+const config = require('../config/index.js');
+const logger = require('./logger.js');
 
 class SecurityManager {
     constructor() {
