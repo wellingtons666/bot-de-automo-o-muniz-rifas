1b8f305ac492514b3f016c5e84549d8fa83948b0
wellingtons666 <wellington.cnstntbtc@gmail.com>
2026-02-21 03:07:50 -0300

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,7 @@
 const pino = require('pino');
 
-const logger = pino({
-    level: process.env.LOG_LEVEL || 'info'
+const logger = pino({ 
+    level: 'silent'  // Silencioso para produção
 });
-
-// Compatibilidade com .child()
-logger.child = function() { return this; };
 
 module.exports = logger;
